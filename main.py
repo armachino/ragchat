b4a1c49a8f447d148d3a803d7ae90c5a7759af4f
arm0n <ranjbararmon@gmail.com>
2025-06-22 15:58:09 +0330

    feat: add main function to run RagAgent with HuggingFace embeddings and Chroma vector store

---
+++ main.py
@@ -0,0 +1,38 @@
+from core.agent import RagAgent
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.chat_models import init_chat_model
+from langchain_chroma import Chroma
+from langchain import hub
+
+
+from dotenv import load_dotenv
+load_dotenv()
+
+def run():
+    """
+    Main function to run the RagAgent.
+    """
+    embedding_model = HuggingFaceEmbeddings(model_name="./models/hf_model")
+    llm = init_chat_model("gemini-2.0-flash", model_provider="google_genai")
+    vector_store = Chroma(
+        collection_name="example_collection",
+        embedding_function=embedding_model,
+        persist_directory="./chroma_langchain_db",  # Where to save data locally, remove if not necessary
+    )
+    prompt = hub.pull("rlm/rag-prompt")
+
+    agent = RagAgent(
+        llm_model=llm,
+        vector_store=vector_store,
+        docs_path="./docs",
+        prompt_template=prompt,
+    )
+    agent.run("Tell me about his Wi-fi Status in the village")  # Example question
+    # Here you can add code to interact with the agent, e.g., sending a question and getting an answer.
+    # For example:
+    # response = agent.run(InputState(question="What is LangGraph?"))
+    # print(response.answer)
+
+
+if __name__ == "__main__":
+    run()
