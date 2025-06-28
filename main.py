e49103bf429fdeff44cc3a4fff8d184d9e7bf1e5
arm0n <ranjbararmon@gmail.com>
2025-06-28 14:08:17 +0330

    feat(main): update run function to use chatbot_collection and simplify example question

--- main.py
+++ main.py
@@ -2,36 +2,32 @@ from core.agent import RagAgent
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.chat_models import init_chat_model
 from langchain_chroma import Chroma
-from langchain import hub
-
+from pprint import pprint
 
 from dotenv import load_dotenv
+
 load_dotenv()
+
 
 def run():
     """
     Main function to run the RagAgent.
     """
+    # You can either download the HuggingFace model or use the already downloaded model from './models/hf_model'
     embedding_model = HuggingFaceEmbeddings(model_name="./models/hf_model")
     llm = init_chat_model("gemini-2.0-flash", model_provider="google_genai")
     vector_store = Chroma(
-        collection_name="example_collection",
+        collection_name="chatbot_collection",
         embedding_function=embedding_model,
         persist_directory="./chroma_langchain_db",  # Where to save data locally, remove if not necessary
     )
-    prompt = hub.pull("rlm/rag-prompt")
 
     agent = RagAgent(
         llm_model=llm,
         vector_store=vector_store,
         docs_path="./docs",
-        prompt_template=prompt,
     )
-    agent.run("Tell me about his Wi-fi Status in the village")  # Example question
-    # Here you can add code to interact with the agent, e.g., sending a question and getting an answer.
-    # For example:
-    # response = agent.run(InputState(question="What is LangGraph?"))
-    # print(response.answer)
+    pprint(agent.run("what is the  Hierarchy of grey wolf?"))  # Example question
 
 
 if __name__ == "__main__":
